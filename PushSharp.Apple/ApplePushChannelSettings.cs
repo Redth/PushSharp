e727b9bfe463a8a674d2a8834cca5272726cec1f
Redth <jondick@gmail.com>
2012-06-26 09:10:02 -0400

    Fixed APNS Feedback Service
    APNS Feedback Service was pointing to the wrong host/port.  This is now
    fixed, and also fixes #4

--- PushSharp.Apple/ApplePushChannelSettings.cs
+++ PushSharp.Apple/ApplePushChannelSettings.cs
@@ -11,8 +11,14 @@ namespace PushSharp.Apple
 		private const string APNS_SANDBOX_HOST = "gateway.sandbox.push.apple.com";
 		private const string APNS_PRODUCTION_HOST = "gateway.push.apple.com";
 
+		private const string APNS_SANDBOX_FEEDBACK_HOST = "feedback.sandbox.push.apple.com";
+		private const string APNS_PRODUCTION_FEEDBACK_HOST = "feedback.push.apple.com";
+
 		private const int APNS_SANDBOX_PORT = 2195;
 		private const int APNS_PRODUCTION_PORT = 2195;
+
+		private const int APNS_SANDBOX_FEEDBACK_PORT = 2196;
+		private const int APNS_PRODUCTION_FEEDBACK_PORT = 2196;
 		#endregion
 
 		public ApplePushChannelSettings(bool production, string certificateFile, string certificateFilePwd) 
@@ -21,7 +27,9 @@ namespace PushSharp.Apple
 		public ApplePushChannelSettings(bool production, byte[] certificateData, string certificateFilePwd)
 		{
 			this.Host = production ? APNS_PRODUCTION_HOST : APNS_SANDBOX_HOST;
+			this.FeedbackHost = production ? APNS_PRODUCTION_FEEDBACK_HOST : APNS_SANDBOX_FEEDBACK_HOST;
 			this.Port = production ? APNS_PRODUCTION_PORT : APNS_SANDBOX_PORT;
+			this.FeedbackPort = production ? APNS_PRODUCTION_FEEDBACK_PORT : APNS_SANDBOX_FEEDBACK_PORT;
 
 			this.CertificateData = certificateData;
 			this.CertificateFilePassword = certificateFilePwd;
@@ -34,6 +42,18 @@ namespace PushSharp.Apple
 		}
 
 		public int Port
+		{
+			get;
+			private set;
+		}
+
+		public string FeedbackHost
+		{
+			get;
+			private set;
+		}
+
+		public int FeedbackPort
 		{
 			get;
 			private set;
