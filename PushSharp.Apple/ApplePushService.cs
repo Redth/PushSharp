5026cfed170f3fbba98dd9ab615fb64e9a563323
Eric Loes <eric.loes@gmail.com>
2012-07-12 11:16:53 -0700

    Added try/catch on Feedback stream connection. To throw ChannelException

--- PushSharp.Apple/ApplePushService.cs
+++ PushSharp.Apple/ApplePushService.cs
@@ -23,8 +23,14 @@ namespace PushSharp.Apple
 			feedbackService.OnFeedbackReceived += new FeedbackService.FeedbackReceivedDelegate(feedbackService_OnFeedbackReceived);
 			timerFeedback = new Timer(new TimerCallback((state) =>
 			{
-				feedbackService.Run(channelSettings as ApplePushChannelSettings, this.cancelTokenSource.Token);
-
+                try
+                {
+                    feedbackService.Run(channelSettings as ApplePushChannelSettings, this.cancelTokenSource.Token);
+                }
+                catch (Exception e)
+                {
+                    this.Events.RaiseChannelException(e);
+                }
 				//Timer will run first after 10 seconds, then every 10 minutes to get feedback!
 			}),null, TimeSpan.FromSeconds(10), TimeSpan.FromMinutes(10));
 
