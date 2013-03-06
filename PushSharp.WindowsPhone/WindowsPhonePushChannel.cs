629d964853e3ac2eea2ab8cf92a0c9c89023d093
Jonathan Dick <jondick@gmail.com>
2013-03-06 13:01:31 -0500

    Revert Unicode/UTF8 change for Windows Phone #114

--- PushSharp.WindowsPhone/WindowsPhonePushChannel.cs
+++ PushSharp.WindowsPhone/WindowsPhonePushChannel.cs
@@ -75,7 +75,7 @@ namespace PushSharp.WindowsPhone
 
 			var payload = wpNotification.PayloadToString();
 
-			var data = Encoding.Unicode.GetBytes(payload);
+			var data = Encoding.UTF8.GetBytes(payload);
 
 			wr.ContentLength = data.Length;
 
