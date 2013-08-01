cd7164036d6a83c2bd813b93c4617c9bd6ce45be
Redth <jondick@gmail.com>
2013-08-01 09:53:45 -0400

    More APNS testing updates.  Tests now control the node.js mock server

--- Tests/APNS-Server-NodeJs/server.js
+++ Tests/APNS-Server-NodeJs/server.js
@@ -1,19 +1,24 @@
 var net     = require("net");
+var http = require("http");
 var util	= require('util');
+var url = require('url');
 var serverparser = require('./lib/server_parser');
 var winston = require('winston');
 var sys = require('sys');
 
 var pushRx = 0;
 var pushRxDiscarded = 0;
-var pushRxSuccess = 0;
-var pushRxFailed = 0;
+var successIds = [];
+var failedIds = [];
 
 
 var ts = function() { 
 		var ts = new Date(); 
 		return ts.getMonth()+1+'-'+ts.getDate()+'-'+ts.getFullYear()+' '+ts.toLocaleTimeString() };
-		
+
+
+var failIds = [];
+
 winston.remove(winston.transports.Console)
 	.add(winston.transports.Console, { level: 'info', colorize: false, timestamp: ts, handleExceptions: true })
 	//.add(winston.transports.File, { level: 'debug', filename: 'error.log', maxsize:5000000, maxFiles:10, json:false, timestamp: ts, handleExceptions: true })
@@ -28,36 +33,34 @@ var pushServer = net.createServer(function(sock) {
     sock.serverParser = new serverparser(sock);
 	sock.serverParser.on('push', function(push) {
 
-        var pid = 0;
-        pid = push.identifier;
+        pushRx++;
 
-        if (pid == 5000 || pid == 15000 || pid == 25000 || pid == 35000 || pid == 45000 || pid == 55000 || pid == 65000 || pid == 75000 || pid == 85000 || pid == 95000)
+        //var pid = 0;
+        var pid = push.identifier;
+        var success = true;
+
+        //See if our id is in the fail list
+        for (var i = 0; i < failIds.length; i++)
         {
-            isClosing = true;
-            success = false;
-            winston.info('FAILING PUSH: ' + pid);
-            sock.serverParser.raiseError(4, pid);
+            if (failIds[i] == pid)
+            {
+                failedIds.push(pid);
+                success = false;
+                isClosing = true;
+                winston.info('! FAILING id: ' + pid + ', deviceToken: ' + push.deviceToken);
+                sock.serverParser.raiseError(4, pid);
+            }
         }
 
-        if (isClosing)
-		    winston.info('! id: ' + pid + ', deviceToken: ' + push.deviceToken);
-        //else
-        //    winston.info('+ id: ' + pid + ', deviceToken: ' + push.deviceToken);
-
-		var success = true;
-
-
-        if (!isClosing)
+        if (!isClosing && success)
         {
-            pushRx++;
-		    if (success)
-			    pushRxSuccess++;
-		    else
-		    	pushRxFailed++;
+		    successIds.push(pid);
+            winston.info('+ SUCCESS id: ' + pid + ', deviceToken: ' + push.deviceToken);
         }
-        else
+        else if (isClosing && success)
         {
             pushRxDiscarded++;
+            winston.info('~ DISCARD id: ' + pid + ', deviceToken: ' + push.deviceToken);
         }
 	});
 	sock.on('data', function(data) {
@@ -76,10 +79,79 @@ var pushServer = net.createServer(function(sock) {
 });
 
 pushServer.listen(2195, function() {
-	winston.info('server listener running');
+	winston.info('APNS Server is listening');
 	}
 );
+
+
+var httpServer = http.createServer(function(request, response) {
+
+    var urlParts = url.parse(request.url, true);
+
+    if (urlParts.pathname === '/setup')
+    {
+        failIds = [];
+        if (urlParts.query && urlParts.query.failId)
+        {
+            for (var i = 0; i < urlParts.query.failId.length; i++) {
+                failIds.push(urlParts.query.failId[i]);
+            }
+        }
+
+        winston.info('setup: failIds: ' + JSON.stringify(failIds));
+
+        var info = {};
+        info.failIds = failIds;
+        info.status = "OK";
+
+        response.writeHead(200, {"Content-Type": "application/javascript"});
+        response.write(JSON.stringify(info));
+        response.end();
+    }
+    else if (urlParts.pathname === '/info')
+    {
+        winston.info('Rx: ' + pushRx + ' (Discarded: ' + pushRxDiscarded +'), Success: ' + successIds.length + ', Failed: ' + failedIds.length);
+
+        var info = {};
+
+        info.status = "OK";
+        info.received = pushRx;
+        info.successIds = successIds;
+        info.failedIds = failedIds;
+        info.discarded = pushRxDiscarded;
+
+        response.writeHead(200, {"Content-Type": "application/javascript"});
+        response.write(JSON.stringify(info));
+        response.end();
+    }
+    else if (urlParts.pathname === '/reset')
+    {
+        winston.info('Reset Stats');
+
+        pushRx = 0;
+        pushRxDiscarded = 0;
+        successIds = [];
+        failedIds = [];
+
+        var info = {};
+        info.status = "OK";
+
+        response.writeHead(200, {"Content-Type": "application/javascript"});
+        response.write(JSON.stringify(info));
+        response.end();
+    }
+
+});
+httpServer.listen(8888, function() {
+    winston.info("HTTP Server is listening");
+});
+
+
+
+
 var stdin = process.openStdin();
+
+
 
 stdin.addListener("data", function(d) {
     // note:  d is an object, and when converted to a string it will
@@ -95,11 +167,11 @@ stdin.addListener("data", function(d) {
     if (line === 'reset')
     {
     	pushRx = 0;
-    	pushRxFailed = 0;
-    	pushRxSuccess = 0;
-        pushRxDiscarded = 0;
+    	pushRxDiscarded = 0;
+        successIds = [];
+        failedIds = [];
     }
     
   });
 
-winston.info('Server running');
+winston.info('Fin. starting');
--- Tests/PushSharp.Tests/AppleTests.cs
+++ Tests/PushSharp.Tests/AppleTests.cs
@@ -35,18 +35,6 @@ namespace PushSharp.Tests
 		}
 
 		[Test]
-		public void APNS_All_ShouldFail_VeryMany()
-		{
-			var amt = 100000;
-
-			var failIndexes = new List<int> ();
-			for (int i = 0; i < amt; i++)
-				failIndexes.Add (i);
-
-			TestNotifications(amt, 0, amt, failIndexes.ToArray(), false);
-		}
-
-		[Test]
 		public void APNS_Some_ShouldFail_VeryMany()
 		{
 			var amt = 100000;
@@ -56,13 +44,29 @@ namespace PushSharp.Tests
 		}
 
 		[Test]
+		public void APNS_All_ShouldSucceed_VeryMany_AutoScale()
+		{
+			TestNotifications(100000, 100000, 0, null, false, true);
+		}
+
+		[Test]
+		public void APNS_Some_ShouldFail_VeryMany_AutoScale()
+		{
+			var amt = 100000;
+			var fail = new int[] { 5000, 15000, 25000, 35000, 45000, 55000, 65000, 75000, 85000, 95000 };
+
+			TestNotifications (amt, amt - fail.Length, fail.Length, fail, false, true);
+		}
+
+
+		[Test]
 		public void APNS_All_ShouldSucceed()
 		{
 			TestNotifications(10, 10, 0);
 		}
 
 		[Test]
-		public void APNS_All_ShouldFaile()
+		public void APNS_All_ShouldFail()
 		{
 			var toFail = new int[10];
 
@@ -102,143 +106,227 @@ namespace PushSharp.Tests
 			TestNotifications(1, 1, 0);
 		}
 
-		public void TestNotifications(int toQueue, int expectSuccessful, int expectFailed, int[] indexesToFail = null, bool waitForScaling = false)
+		public void TestNotifications(int toQueue, int expectSuccessful, int expectFailed, int[] idsToFail = null, bool waitForScaling = false, bool autoScale = false)
 		{
+			var testServer = new ApnsNodeTestServer ("http://localhost:8888/");
+			testServer.Reset ();
+			testServer.Setup (idsToFail ?? new int[] {});
+
 			var started = DateTime.UtcNow;
 
-			testPort++;
-					
 			int pushFailCount = 0;
 			int pushSuccessCount = 0;
-			
-			int serverReceivedCount = 0;
-			int serverReceivedFailCount = 0;
-			int serverReceivedSuccessCount = 0;
-			int lastIdentifier = -1;
 
 			AppleNotification.ResetIdentifier ();
 
-			var notification = new AppleNotification("aff441e214b2b2283df799f0b8b16c17a59b7ac077e2867ea54ebf6086e55866").WithAlert("Test");
-
-			var len = notification.ToBytes().Length;
-
-			var server = new TestServers.ApnsTestServer();
-			
-			server.ResponseFilters.Add(new ApnsResponseFilter()
-			{
-				IsMatch = (identifier, token, payload) =>
-				{
-					var id = identifier;
-
-					if (token.StartsWith("b", StringComparison.InvariantCultureIgnoreCase))
-					{
-						Console.WriteLine("Failing: " + identifier);
-						return true;
-					}
-
-					return false;
-				},
-				Status = ApnsResponseStatus.InvalidToken
-			});
-
-			var waitServerFinished = new ManualResetEvent(false);
-
-
-			Task.Factory.StartNew(() =>
-				{
-					try
-					{
-						server.Start(testPort, len, (success, identifier, token, payload) =>
-						{
-                            //Console.WriteLine("Server Received: id=" + identifier + ", payload= " + payload + ", token=" + token + ", success=" + success);
-							
-							if (identifier - lastIdentifier > 1)
-
-							serverReceivedCount++;
-
-							if (success)
-								serverReceivedSuccessCount++;
-							else
-								serverReceivedFailCount++;
-						});
-
-					}
-					catch (Exception ex)
-					{
-						Console.WriteLine(ex);										
-					}
-
-					waitServerFinished.Set();
-
-				}).ContinueWith(t =>
-					{
-						var ex = t.Exception;
-						Console.WriteLine(ex);
-					}, TaskContinuationOptions.OnlyOnFaulted);
-
 			var settings = new ApplePushChannelSettings(false, appleCert, "pushsharp", true);
-			//settings.OverrideServer("localhost", testPort);
 			settings.OverrideServer("localhost", 2195);
 			settings.SkipSsl = true;
-			//settings.MillisecondsToWaitBeforeMessageDeclaredSuccess = 60000 * 5;
+			settings.MillisecondsToWaitBeforeMessageDeclaredSuccess = 5000;
 
+			var serviceSettings = new PushServiceSettings();
 
-			var push = new ApplePushService(settings, new PushServiceSettings() { AutoScaleChannels = false, Channels = 1 });
+			if (!autoScale)
+			{
+				serviceSettings.AutoScaleChannels = false;
+				serviceSettings.Channels = 1;
+			}
+
+			var push = new ApplePushService(settings, serviceSettings);
 			push.OnNotificationFailed += (sender, notification1, error) => {
-				Console.WriteLine("APPLEPUSHSERVICE NOTIFICATION FAILED: " + ((AppleNotification)notification1).Identifier);
+				Console.WriteLine("NOTIFICATION FAILED: " + ((AppleNotification)notification1).Identifier);
 				pushFailCount++;
 			};
 			push.OnNotificationSent += (sender, notification1) => {
 				pushSuccessCount++;
 			};
+			push.OnNotificationRequeue += (sender, e) => {
+				Console.WriteLine("REQUEUE: " + ((AppleNotification)e.Notification).Identifier);
+			};
 
 			for (int i = 0; i < toQueue; i++)
 			{
-				INotification n;
+				var n = new AppleNotification("aff441e214b2b2283df799f0b8b16c17a59b7ac077e2867ea54ebf6086e55866").WithAlert("Test");
 
-				if (indexesToFail != null && indexesToFail.Contains(i))
-					n = new AppleNotification("bff441e214b2b2283df799f0b8b16c17a59b7ac077e2867ea54ebf6086e55866").WithAlert("Test");
-				else
-					n = new AppleNotification("aff441e214b2b2283df799f0b8b16c17a59b7ac077e2867ea54ebf6086e55866").WithAlert("Test");
-				
 				push.QueueNotification(n);
 			}
 
-            Console.WriteLine("Avg Queue Wait Time: " + push.AverageQueueWaitTime + " ms");
-            Console.WriteLine("Avg Send Time: " + push.AverageSendTime + " ms");
+			if (waitForScaling)
+			{
+				while (push.QueueLength > 0)
+					Thread.Sleep(500);
 
-            if (waitForScaling)
-            {
-                while (push.QueueLength > 0)
-                    Thread.Sleep(500);
+				Console.WriteLine("Sleeping 3 minutes for autoscaling...");
+				Thread.Sleep(TimeSpan.FromMinutes(3));
 
-                Console.WriteLine("Sleeping 3 minutes for autoscaling...");
-                Thread.Sleep(TimeSpan.FromMinutes(3));
-
-                Console.WriteLine("Channel Count: " + push.ChannelCount);
-                Assert.IsTrue(push.ChannelCount <= 1);
-            }
+				Console.WriteLine("Channel Count: " + push.ChannelCount);
+				Assert.IsTrue(push.ChannelCount <= 1);
+			}
 
 			push.Stop();
 			push.Dispose();
 
-			server.Dispose();
-			//waitServerFinished.WaitOne();
-
-			Console.WriteLine("TEST-> DISPOSE.");
+			Console.WriteLine("Avg Queue Wait Time: " + push.AverageQueueWaitTime + " ms");
+			Console.WriteLine("Avg Send Time: " + push.AverageSendTime + " ms");
 
 			var span = DateTime.UtcNow - started;
 
+			var info = testServer.GetInfo ();
+
 			Console.WriteLine ("Test Time: " + span.TotalMilliseconds + " ms");
 			Console.WriteLine ("Client Failed: {0} Succeeded: {1} Sent: {2}", pushFailCount, pushSuccessCount, toQueue);
-			Console.WriteLine ("Server Failed: {0} Succeeded: {1} Received: {2}", serverReceivedFailCount, serverReceivedSuccessCount, serverReceivedCount);
-			
-			//Assert.AreEqual(toQueue, serverReceivedCount, "Server - Received Count");
-			//Assert.AreEqual(expectFailed, serverReceivedFailCount, "Server - Failed Count");
-			//Assert.AreEqual(expectSuccessful, serverReceivedSuccessCount, "Server - Success Count");
+			Console.WriteLine ("Server Failed: {0} Succeeded: {1} Received: {2} Discarded: {3}", info.FailedIds.Length, info.SuccessIds.Length, info.Received, info.Discarded);
+
+			//Assert.AreEqual(toQueue, info.Received, "Server - Received Count");
+			Assert.AreEqual(expectFailed, info.FailedIds.Length, "Server - Failed Count");
+			Assert.AreEqual(expectSuccessful, info.SuccessIds.Length, "Server - Success Count");
 
 			Assert.AreEqual(expectFailed, pushFailCount, "Client - Failed Count");
 			Assert.AreEqual(expectSuccessful, pushSuccessCount, "Client - Success Count");
 		}
+
+//		public void TestNotifications(int toQueue, int expectSuccessful, int expectFailed, int[] idsToFail = null, bool waitForScaling = false)
+//		{
+//			var started = DateTime.UtcNow;
+//
+//			testPort++;
+//					
+//			int pushFailCount = 0;
+//			int pushSuccessCount = 0;
+//			
+//			int serverReceivedCount = 0;
+//			int serverReceivedFailCount = 0;
+//			int serverReceivedSuccessCount = 0;
+//			int lastIdentifier = -1;
+//
+//			AppleNotification.ResetIdentifier ();
+//
+//			/*var notification = new AppleNotification("aff441e214b2b2283df799f0b8b16c17a59b7ac077e2867ea54ebf6086e55866").WithAlert("Test");
+//
+//			var len = notification.ToBytes().Length;
+//
+//			var server = new TestServers.ApnsTestServer();
+//			
+//			server.ResponseFilters.Add(new ApnsResponseFilter()
+//			{
+//				IsMatch = (identifier, token, payload) =>
+//				{
+//					var id = identifier;
+//
+//					if (token.StartsWith("b", StringComparison.InvariantCultureIgnoreCase))
+//					{
+//						Console.WriteLine("Failing: " + identifier);
+//						return true;
+//					}
+//
+//					return false;
+//				},
+//				Status = ApnsResponseStatus.InvalidToken
+//			});
+//			*/
+//
+//			/*var waitServerFinished = new ManualResetEvent(false);
+//
+//
+//			Task.Factory.StartNew(() =>
+//				{
+//					try
+//					{
+//						server.Start(testPort, len, (success, identifier, token, payload) =>
+//						{
+//                            //Console.WriteLine("Server Received: id=" + identifier + ", payload= " + payload + ", token=" + token + ", success=" + success);
+//							
+//							if (identifier - lastIdentifier > 1)
+//
+//							serverReceivedCount++;
+//
+//							if (success)
+//								serverReceivedSuccessCount++;
+//							else
+//								serverReceivedFailCount++;
+//						});
+//
+//					}
+//					catch (Exception ex)
+//					{
+//						Console.WriteLine(ex);										
+//					}
+//
+//					waitServerFinished.Set();
+//
+//				}).ContinueWith(t =>
+//					{
+//						var ex = t.Exception;
+//						Console.WriteLine(ex);
+//					}, TaskContinuationOptions.OnlyOnFaulted);
+//			*/
+//
+//			var settings = new ApplePushChannelSettings(false, appleCert, "pushsharp", true);
+//			//settings.OverrideServer("localhost", testPort);
+//			settings.OverrideServer("localhost", 2195);
+//			settings.SkipSsl = true;
+//			//settings.MillisecondsToWaitBeforeMessageDeclaredSuccess = 60000 * 5;
+//
+//
+//			var push = new ApplePushService(settings, new PushServiceSettings() { AutoScaleChannels = false, Channels = 1 });
+//			push.OnNotificationFailed += (sender, notification1, error) => {
+//				Console.WriteLine("APPLEPUSHSERVICE NOTIFICATION FAILED: " + ((AppleNotification)notification1).Identifier);
+//				pushFailCount++;
+//			};
+//			push.OnNotificationSent += (sender, notification1) => {
+//				pushSuccessCount++;
+//			};
+//			push.OnNotificationRequeue += (sender, e) => {
+//				Console.WriteLine("REQUEUE: " + ((AppleNotification)e.Notification).Identifier);
+//			};
+//
+//			for (int i = 0; i < toQueue; i++)
+//			{
+//				//INotification n;
+//
+//				//if (idsToFail != null && idsToFail.Contains(i))
+//				//	n = new AppleNotification("bff441e214b2b2283df799f0b8b16c17a59b7ac077e2867ea54ebf6086e55866").WithAlert("Test");
+//				//else
+//				var n = new AppleNotification("aff441e214b2b2283df799f0b8b16c17a59b7ac077e2867ea54ebf6086e55866").WithAlert("Test");
+//				
+//				push.QueueNotification(n);
+//			}
+//
+//            Console.WriteLine("Avg Queue Wait Time: " + push.AverageQueueWaitTime + " ms");
+//            Console.WriteLine("Avg Send Time: " + push.AverageSendTime + " ms");
+//
+//            if (waitForScaling)
+//            {
+//                while (push.QueueLength > 0)
+//                    Thread.Sleep(500);
+//
+//                Console.WriteLine("Sleeping 3 minutes for autoscaling...");
+//                Thread.Sleep(TimeSpan.FromMinutes(3));
+//
+//                Console.WriteLine("Channel Count: " + push.ChannelCount);
+//                Assert.IsTrue(push.ChannelCount <= 1);
+//            }
+//
+//			push.Stop();
+//			push.Dispose();
+//
+//			//server.Dispose();
+//			//waitServerFinished.WaitOne();
+//
+//			Console.WriteLine("TEST-> DISPOSE.");
+//
+//			var span = DateTime.UtcNow - started;
+//
+//			Console.WriteLine ("Test Time: " + span.TotalMilliseconds + " ms");
+//			Console.WriteLine ("Client Failed: {0} Succeeded: {1} Sent: {2}", pushFailCount, pushSuccessCount, toQueue);
+//			Console.WriteLine ("Server Failed: {0} Succeeded: {1} Received: {2}", serverReceivedFailCount, serverReceivedSuccessCount, serverReceivedCount);
+//			
+//			//Assert.AreEqual(toQueue, serverReceivedCount, "Server - Received Count");
+//			//Assert.AreEqual(expectFailed, serverReceivedFailCount, "Server - Failed Count");
+//			//Assert.AreEqual(expectSuccessful, serverReceivedSuccessCount, "Server - Success Count");
+//
+//			Assert.AreEqual(expectFailed, pushFailCount, "Client - Failed Count");
+//			Assert.AreEqual(expectSuccessful, pushSuccessCount, "Client - Success Count");
+//		}
 	}
 }
