47cb6f3d62f52dcc51da1de83ec5962208553430
Redth <jondick@gmail.com>
2015-10-27 10:38:38 -0400

    Added real test for APNS

---
+++ PushSharp.Tests/ApnsRealTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using PushSharp.Apple;
+using Newtonsoft.Json.Linq;
+
+namespace PushSharp.Tests
+{
+    [TestFixture]
+    [Category ("Disabled")]
+    public class ApnsRealTest
+    {
+        [Test]
+        public void APNS_Send_Single ()
+        {
+            var succeeded = 0;
+            var failed = 0;
+            var attempted = 0;
+
+            var config = new ApnsConfiguration (Settings.Instance.ApnsCertificateFile, Settings.Instance.ApnsCertificatePassword);
+            var broker = new ApnsServiceBroker (config);
+            broker.OnNotificationFailed += (notification, exception) => {
+                failed++;
+            };
+            broker.OnNotificationSucceeded += (notification) => {
+                succeeded++;  
+            };
+            broker.Start ();
+
+            foreach (var dt in Settings.Instance.ApnsDeviceTokens) {
+                attempted++;
+                broker.QueueNotification (new ApnsNotification {
+                    DeviceToken = dt,
+                    Payload = JObject.Parse ("{ \"aps\" : { \"alert\" : \"Hello PushSharp!\" } }")
+                });
+            }
+
+            broker.Stop ();
+
+            Assert.AreEqual (attempted, succeeded);
+            Assert.AreEqual (0, failed);
+        }
+    }
+}
+
