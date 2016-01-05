eae97b7908b8db3290c3a3e1c37df531b1e35433
Redth <jondick@gmail.com>
2016-01-05 16:35:11 -0500

    Check for matching uuid's in request/response to apns http/2

--- PushSharp.Apple/ApnsHttp2Connection.cs
+++ PushSharp.Apple/ApnsHttp2Connection.cs
@@ -85,13 +85,13 @@ namespace PushSharp.Apple
                 headers.Add ("apns-topic", notification.Topic); // string topic
 
             var response = await http2.Post (uri, headers, data);
-
-            // Check for matching uuid's
-            //var responseUuid = response.Headers ["apns-id"];
-            //if (responseUuid != notification.Uuid)
-            //    throw new Exception ("Mismatched APNS-ID header values");
-
+            
             if (response.Status == System.Net.HttpStatusCode.OK) {
+                // Check for matching uuid's
+                var responseUuid = response.Headers ["apns-id"];
+                if (responseUuid != notification.Uuid)
+                    throw new Exception ("Mismatched APNS-ID header values");
+                
                 Console.WriteLine("OK");
             } else {
                 // Try parsing json body
