a9c5a61a47a09aa29c69d037ad4a01ac690540d8
Redth <jondick@gmail.com>
2016-03-05 14:40:51 -0500

    Manually merged PR.  Closes #599
    Thanks for this, I tweaked it a bit to fit my code style.  Can you please verify this works properly for you?

--- PushSharp.Tests/WnsRealTests.cs
+++ PushSharp.Tests/WnsRealTests.cs
@@ -38,12 +38,57 @@ namespace PushSharp.Tests
                         <toast>
                             <visual>
                                 <binding template=""ToastText01"">
-                                    <text id=""1"">bodyText</text>
+                                    <text id=""1"">WNS_Send_Single</text>
                                 </binding>  
                             </visual>
                         </toast>
                     ")
                 });
+            }
+
+            broker.Stop ();
+
+            Assert.AreEqual (attempted, succeeded);
+            Assert.AreEqual (0, failed);
+        }
+
+        [Test]
+        public void WNS_Send_Mutiple ()
+        {
+            var succeeded = 0;
+            var failed = 0;
+            var attempted = 0;
+
+            var config = new WnsConfiguration (Settings.Instance.WnsPackageName,
+                Settings.Instance.WnsPackageSid,
+                Settings.Instance.WnsClientSecret);
+
+            var broker = new WnsServiceBroker (config);
+            broker.OnNotificationFailed += (notification, exception) => {
+                failed++;
+            };
+            broker.OnNotificationSucceeded += (notification) => {
+                succeeded++;
+            };
+
+            broker.Start ();
+
+            foreach (var uri in Settings.Instance.WnsChannelUris) {
+                for (var i = 1; i <= 3; i++) {
+                    attempted++;
+                    broker.QueueNotification (new WnsToastNotification {
+                            ChannelUri = uri,
+                            Payload = XElement.Parse(@"
+                                <toast>
+                                    <visual>
+                                        <binding template=""ToastText01"">
+                                            <text id=""1"">WNS_Send_Multiple " + i.ToString() + @"</text>
+                                        </binding>  
+                                    </visual>
+                                </toast>
+                            ")
+                        });
+                }
             }
 
             broker.Stop ();
